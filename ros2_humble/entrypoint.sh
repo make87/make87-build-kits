1537963256658eacc08ddcb288e3004b299dece3
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-07-28 16:39:40 -0700

    Removed legacy ROS2 build kits

--- ros2_humble/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Source the ROS 2 setup scripts
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/app/install/setup.bash"
-
-# Execute the command passed to the container
-source "$@"
